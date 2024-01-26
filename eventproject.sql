a96ddf4a02c2a33bd528a43ae83cb97b230ac635
test <test@example.com>
2024-01-26 11:42:07 -0600

    added tables

--- eventproject.sql
+++ eventproject.sql
@@ -1,5 +1,25 @@
 CREATE TABLE Speakers (
 SpeakerName VARCHAR(50),
-Bio VARCHAR(5000),
+Bio VARCHAR(1000),
 ContactInfo VARCHAR(100)
+);
+
+CREATE TABLE Events (
+Title VARCHAR(100),
+DateAndTime DATETIME,
+Description VARCHAR(1000)
+);
+
+CREATE TABLE Venues (
+VenueName VARCHAR(100),
+Location VARCHAR(50),
+Capacity INT UNSIGNED,
+ContactInfo VARCHAR(100)
+);
+
+CREATE TABLE Attendees (
+AttendeeName VARCHAR(50),
+Email VARCHAR(100),
+RegistrationDate DATE,
+EventAttended VARCHAR(100)
 );
